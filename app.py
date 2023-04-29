91e07d5a25208d7f620aeae59b3e8d0e31fe7e0f
Uzair Mansoor <uzairmansoor123123@gmail.com>
2023-04-30 01:22:54 +0500

    demoproject
    MyFinalProject

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def final_project():
+   target = os.environ.get('TARGET', 'HELLO Everyone !!')
+   return 'Welcome to Our Final Project, {}!'.format(target)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=int(os.environ.get('PORT',8080)), debug=True)
