b9493b75f0dd6c27e9b6429e345f865d4a0c3723
mohamm29 <mohammedfarazahmed@valpo.edu>
2023-05-01 07:31:06 +0000

    hello

---
+++ app.py
@@ -0,0 +1,17 @@
+
+import os
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def final_project():
+   target = os.environ.get('TARGET', 'HELLO Everyone !!')
+   return 'Welcome to Our IT-590  Final Project, {}!'.format(target)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=int(os.environ.get('PORT',8080)), debug=True)
